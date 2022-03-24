5704a2dce92c02d7efde34aab71b8a6120db2e61
rajdashora <56566501+rajdashora@users.noreply.github.com>
2022-03-24 00:55:03 -0500

    add vehicle state subscriber

--- workspace/src/03_driving_functions/driving_functions_py/driving_functions_py/planning_node.py
+++ workspace/src/03_driving_functions/driving_functions_py/driving_functions_py/planning_node.py
@@ -10,6 +10,7 @@ appropriate trajectories.
 
 Subscribers
 /localization/state              - Track
+/localization/state              - VehicleState
 
 Publishers
 /control/planning                - Path
@@ -21,9 +22,10 @@ The trajectory will be published in state-space
 import rclpy
 from rclpy.parameter import Parameter
 from rclpy.node import Node
-from CenterlinePlanner import CenterlinePlanner
+from driving_functions_py.CenterlinePlanner import CenterlinePlanner
 import numpy as np
-from wagrandprix_map_msgs.msg import Track, Point
+from wagrandprix_map_msgs.msg import TrackBoundaries, Point
+from wagrandprix_state_msgs.msg import VehicleState
 from wagrandprix_control_msgs.msg import Path
 
 ## Class to generate and publish path msgs
@@ -38,21 +40,29 @@ class PlanningNode(Node):
         self.msg_path = Path()
         # Create publisher
         self.pub_path = self.create_publisher(Path, '/control/planning', 1)
-        # Create subscriber
-        self.sub_state = self.create_subscription(Track, '/localization/track', self._receive_track, 1)
+        # Create subscribers
+        self.sub_track = self.create_subscription(TrackBoundaries, '/localization/track', self._receive_track, 1)
+        self.sub_state = self.create_subscription(VehicleState, '/localization/state', self._receive_state, 1)
         # Create Centerline Planner class
         self.cp = CenterlinePlanner()
 
+        self.timer = self.create_timer(0.5, self.send_waypoint)
         self.received_Track = False
+        self.received_State = False
 
     # Recieve set of track waypoints
     def _receive_track(self,msg):
         self.received_Track = True
-        self.track = msg.waypoints
+        self.track = msg
+
+    # Recieve vehicle state
+    def _receive_state(self,msg):
+        self.received_State = True
+        self.state = msg
 
     # Publish next setpoint
-    def send_path(self):
-        if self.received_Track:
+    def send_waypoint(self):
+        if self.received_Track and self.received_State:
             # get path
             waypoints = self.cp.get_path(self.track)
 
