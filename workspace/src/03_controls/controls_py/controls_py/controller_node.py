d7f92ef076708ccc594448a9e10474a716448b8c
Sriram Ashokkumar <srira@LAPTOP-QQ6RBHMU.localdomain>
2022-04-29 09:26:04 -0500

    restructured the control node

--- workspace/src/03_controls/controls_py/controls_py/controller_node.py
+++ workspace/src/03_controls/controls_py/controls_py/controller_node.py
@@ -1,25 +1,10 @@
 #! /usr/bin/env python3
 
+__author__ = "Sriram Ashokkumar"
+__email__ = "ashokkumar2@wisc.edu"
+
 __author__ = "Victor Freire"
 __email__ = "freiremelgiz@wisc.edu"
-
-"""
-This node uses the classes in Controllers.py and provides
-an interface to the rest of the ROS2 network. The node will
-not start publishing control inputs until it has received a
-message in /controller/trajectory
-
-This node takes 1 argument:
-1 - mode: controller type (required)
-
-Subscribers:
-    /localization/state - VehicleState
-    /control/planning - Point
-
-Publishers:
-    /control/input - VehicleCommand
-
-"""
 
 import rclpy
 from rclpy.parameter import Parameter
@@ -35,35 +20,108 @@ import wa_simulator as wa
 class ControllerNode(Node):
     def __init__(self):
         super().__init__('controller_node')
-        # Use sim time by default
-        # sim_time = Parameter('use_sim_time', Parameter.Type.BOOL, True)
-        # self.set_parameters([sim_time])
 
-        # self.controller = StanleyController(VehicleState(), [0,0,0]) #need to add target point info
+        self.logger = rclpy.logging.get_logger(self.get_name())
+
+        # ------------
+        # Parse params
+        # ------------
+        SteeringCommand_descriptor = ParameterDescriptor(type=ParameterType.PARAMETER_STRING, description="The topic that SteeringCommand will be shipped on.")
+        self.declare_parameter("SteeringCommand", "/control/steering", SteeringCommand_descriptor)
+        self.target_point_topic = self.get_parameter("SteeringCommand").value
+
+        BrakingCommand_descriptor = ParameterDescriptor(type=ParameterType.PARAMETER_STRING, description="The topic that BrakingCommand will be shipped on.")
+        self.declare_parameter("BrakingCommand", "/control/braking", BrakingCommand_descriptor)
+        self.target_point_topic = self.get_parameter("BrakingCommand").value
+
+        ThrottleCommand_descriptor = ParameterDescriptor(type=ParameterType.PARAMETER_STRING, description="The topic that ThrottleCommand will be shipped on.")
+        self.declare_parameter("ThrottleCommand", "/control/throttle", ThrottleCommand_descriptor)
+        self.target_point_topic = self.get_parameter("ThrottleCommand").value
+
+        target_point_descriptor = ParameterDescriptor(type=ParameterType.PARAMETER_STRING, description="The topic that target point will be shipped on.")
+        self.declare_parameter("target_point", "/control/planning", target_point_descriptor)
+        self.target_point_topic = self.get_parameter("target_point").value
+
+        vehicle_state_topic_descriptor = ParameterDescriptor(type=ParameterType.PARAMETER_STRING, description="The topic that provides vehicle state information")
+        self.declare_parameter("vehicle_state_topic", "/localization/state", vehicle_state_topic_descriptor)
+        self.vehicle_state_topic = self.get_parameter("vehicle_state_topic").value
+
+        fake_with_sim_descriptor = ParameterDescriptor(type=ParameterType.PARAMETER_BOOL, description="Whether to run this nodes algorithms or fake it with sim data.")
+        self.declare_parameter("fake_with_sim", False, fake_with_sim_descriptor)
+        self.fake_with_sim = self.get_parameter("fake_with_sim").value
+
+        self.logger.info(f"fake_with_sim: {self.fake_with_sim}")
+
+
+        # ------------
+        # ROS Entities
+        # ------------
+        
+        # Create publisher handles
+        self.publisher_handles = {}
+
+        self.publisher_handles["/control/steering"] = self.create_publisher(SteeringCommand,'/control/steering',1)
+        self.publisher_handles["/control/braking"] = self.create_publisher(BrakingCommand,'/control/braking',1)
+        self.publisher_handles["/control/throttle"] = self.create_publisher(ThrottleCommand,'/control/throttle',1)
+
+        # Create subcriber handles
+        self.subscriber_handles = {}
+
+        # If desired, we may "fake" the vehicle state estimator node
+        # This can be done using sim, where we grab all of the vehicle state information directly from the simulation
+        self.logger.info(f"vehicle_state_topic: {self.vehicle_state_topic}")
+        self.logger.info(f"target_point_descriptor: {self.target_point_descriptor}")
+
+        self.subscriber_handles[self.vehicle_state_topic] = self.create_subscription(VehicleState, self.vehicle_state_topic, self._save_state, 1)
+        self.subscriber_handles[self.target_point_descriptor] = self.create_subscription(Point, self.target_point_topic, self._save_target, 1)
+        
+        # ------------------------
+        # Initialize Class Members
+        # ------------------------
+        # if == "PID":
+        
+        # if == "StanleyController"":
+
+
         self.controller = StanleyController(wa.WASystem(), VehicleState(), [1,1,1], wa.WAVehicleInputs())
-        # We could just use cars current pos as a placeholder for target to initialize it if we need
-        # So [vehicle_state.pose.position.x, ...y, ...z]
-        # - Raj
 
         self.vehicle_command = VehicleCommand()
-
-        # Subs and Pubs   -----------   Replace with right ones -- done??
-        self.sub_state = self.create_subscription(VehicleState, "/localization/state", self._save_state, 1) #need another one
-        self.sub_state = self.create_subscription(Point, "/control/planning", self._save_target, 1)
-        # self.pub_cmd = self.create_publisher(VehicleCommand,'/control/input',1)
-        self.pub_steering = self.create_publisher(SteeringCommand,'/control/steering',1)
-        self.pub_braking = self.create_publisher(BrakingCommand,'/control/braking',1)
-        self.pub_throttle = self.create_publisher(ThrottleCommand,'/control/throttle',1)
-
-
-        # self.sub_traj = self.create_subscription(CarTrajectory,      --- remove?
-        #         "/control/trajectory", self._save_trajectory, 1)
 
         # Send cmd at 100 Hz
         self.step = 0.01  # just for reference for now
         self.received_VehicleState = False
         self.received_VehicleTarget = False
         self.timer = self.create_timer(0.5, self.send_control)
+
+        # # Use sim time by default
+        # # sim_time = Parameter('use_sim_time', Parameter.Type.BOOL, True)
+        # # self.set_parameters([sim_time])
+
+        # # self.controller = StanleyController(VehicleState(), [0,0,0]) #need to add target point info
+        # self.controller = StanleyController(wa.WASystem(), VehicleState(), [1,1,1], wa.WAVehicleInputs())
+        # # We could just use cars current pos as a placeholder for target to initialize it if we need
+        # # So [vehicle_state.pose.position.x, ...y, ...z]
+        # # - Raj
+
+        # self.vehicle_command = VehicleCommand()
+
+        # # Subs and Pubs   -----------   Replace with right ones -- done??
+        # self.sub_state = self.create_subscription(VehicleState, "/localization/state", self._save_state, 1) #need another one
+        # self.sub_state = self.create_subscription(Point, "/control/planning", self._save_target, 1)
+        # # self.pub_cmd = self.create_publisher(VehicleCommand,'/control/input',1)
+        # self.pub_steering = self.create_publisher(SteeringCommand,'/control/steering',1)
+        # self.pub_braking = self.create_publisher(BrakingCommand,'/control/braking',1)
+        # self.pub_throttle = self.create_publisher(ThrottleCommand,'/control/throttle',1)
+
+
+        # # self.sub_traj = self.create_subscription(CarTrajectory,      --- remove?
+        # #         "/control/trajectory", self._save_trajectory, 1)
+
+        # # Send cmd at 100 Hz
+        # self.step = 0.01  # just for reference for now
+        # self.received_VehicleState = False
+        # self.received_VehicleTarget = False
+        # self.timer = self.create_timer(0.5, self.send_control)
 
         #delete
         # self.fakeValues = [[0, .15, 0],[-.2, .30, 0], [-.4, .60, 0], [-.6, .80, 0], [-.8, 1, 0], [-1, .8, 0], [-.5, 0, 0.1], [0, 0, 0.4], [.3, 0, 0.8], [.6, 0, 1], [.8, 0, 1], [1, 0, 1]]
