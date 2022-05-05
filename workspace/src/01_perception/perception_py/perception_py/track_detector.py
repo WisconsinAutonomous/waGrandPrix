58ef4522b3180cdde12a5a9bbbec1607049fa965
masranber <47961042+masranber@users.noreply.github.com>
2022-05-04 19:35:22 -0500

    darknet and point cloud processing in perception node

--- workspace/src/01_perception/perception_py/perception_py/track_detector.py
+++ workspace/src/01_perception/perception_py/perception_py/track_detector.py
@@ -8,6 +8,22 @@ from sensor_msgs.msg import Image, PointCloud2
 from geometry_msgs.msg import PoseArray
 from wagrandprix_map_msgs.msg import DetectedTrack, Point
 
+import numpy as np
+import sensor_msgs.point_cloud2 as pc2
+
+DARKNET_BATCH_SIZE = 1
+DARKNET_THRESH = 0.6
+DARKNET_CONFIG_PATH = '<todo>/cfg/yolov4-tiny-evgrandprix22.cfg'
+DARKNET_WEIGHTS_PATH = '<todo>/backup/evgrandprix22/yolov4-tiny-evgrandprix22_last.weights'
+DARKNET_DATA_PATH = '<todo>/data/evgrandprix22/evgrandprix22.data'
+
+network = None
+network_width, network_height = None, None
+network_img_gpu = None
+class_names = None
+class_colors = None
+
+
 class TrackDetector(Node):
 
     def __init__(self):
@@ -62,21 +78,49 @@ class TrackDetector(Node):
             # self.subscriber_handles[self.camera_topic] = self.create_subscription(Image, self.camera_topic, self.camera_callback, 1)
             self.subscriber_handles[self.pointcloud_topic] = self.create_subscription(PointCloud2, self.pointcloud_topic, self.pointcloud_callback, 1)
 
-        # ------------------------
-        # Initialize Class Members
-        # ------------------------
-
-    # def camera_callback(self, msg):
-    #     """
-    #     Callback for the camera image topic.
-    #     """
-    #     self.logger.debug(f"Received {msg} on topic {self.camera_topic}")
+    # ------------------------
+    # Initialize Class Members
+    # ------------------------
+      
+      
+    def deserialize_zed_pc2_to_numpy(self, pc_msg):
+        image_list = []
+        depth_list = []
+        
+        for p in pc2.read_points(msg):
+            image_list.append((p[0], p[1], p[2])) # read r,g,b from ZED depth cloud point
+            depth_list.append((p[3], p[4], p[5])) # read x,y,z from ZED depth cloud point
+            
+        image_np = np.array(image_list)
+        depth_np = np.array(depth_list)
+        
+        return (image_np, depth_np)
+        
+        
+    def preprocess_image(self, image):
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_resized = cv2.resize(image_rgb, (network_width, network_height), interpolation=cv2.INTER_LINEAR) # resize ZED image to size network expects (416x416)
+        return image_resized
+        
 
     def pointcloud_callback(self, msg):
         """
         Callback for the zed pointcloud topic.
         """
         self.logger.debug(f"Received {msg} on topic {self.pointcloud_topic}")
+        
+        raw_image, depth_cloud = deserialize_zed_pc2_to_numpy(msg)
+        
+        image = preprocess_image(raw_image)
+        
+        darknet.copy_image_from_bytes(network_img_gpu, image.tobytes()) # copy image data to GPU
+        
+        detections = darknet.detect_image(network, class_names, network_img_gpu, thresh=DARKNET_THRESH)
+
+        
+
+        detected_track = DetectedTrack(left_points=msg.left_points, right_points=msg.right_points)
+        self.publisher_handles["detected/track"].publish(detected_track)
 
     def sim_track_callback(self, msg):
         """
@@ -86,11 +130,40 @@ class TrackDetector(Node):
 
         detected_track = DetectedTrack(left_points=msg.left_points, right_points=msg.right_points)
         self.publisher_handles["detected/track"].publish(detected_track)
+        
+        
+
+def check_darknet_args(thresh, config_path, weights_path, data_path):
+    assert 0 < thresh < 1, "Threshold should be a float between zero and one (non-inclusive)"
+    if not os.path.exists(config_path):
+        raise(ValueError("Invalid config path {}".format(os.path.abspath(config_path))))
+    if not os.path.exists(weights_path):
+        raise(ValueError("Invalid weight path {}".format(os.path.abspath(weights_path))))
+    if not os.path.exists(data_path):
+        raise(ValueError("Invalid data file path {}".format(os.path.abspath(data_path))))
+
+def init_model():
+    check_darknet_args(DARKNET_THRESH, DARKNET_CONFIG_PATH, DARKNET_WEIGHTS_PATH, DARKNET_DATA_PATH)
+    
+    network, class_names, class_colors = darknet.load_network(
+            DARKNET_CONFIG_PATH,
+            DARKNET_DATA_PATH,
+            DARKNET_WEIGHTS_PATH,
+            batch_size=DARKNET_BATCH_SIZE
+    )
+    
+    network_width = darknet.network_width(network)
+    network_height = darknet.network_height(network)
+    
+    network_img_gpu = darknet.make_image(network_width, network_height, 3)
+    
+    self.logger.debug('Initialized YOLOv4 (darknet) model')
 
 
 def main(args=None):
     rclpy.init(args=args)
-
+    
+    init_model()
     track_detector = TrackDetector()
 
     rclpy.spin(track_detector)
